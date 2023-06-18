99d6876e6a4905128122b7e309bb8494c8852b8a
AvDuy <avandduy@gmail.com>
2023-06-18 07:26:09 +0700

    Update 1.0.3

--- ShopXeDap(ASM)/src/java/dao/BaseDAO.java
+++ ShopXeDap(ASM)/src/java/dao/BaseDAO.java
@@ -9,29 +9,68 @@ package dao;
  * @author AD
  */
 
+import context.DBContext;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import model.Category;
+import model.Product;
 
 
-public abstract class BaseDAO<T> {
-    protected Connection connection;
-    public BaseDAO()
-    {
-        try {
-            String user = "sa";
-            String pass = "12345678";
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=shopxedap;integratedSecurity=true";
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            connection = DriverManager.getConnection(url, user, pass);
-        } catch (ClassNotFoundException | SQLException ex) {
-            Logger.getLogger(BaseDAO.class.getName()).log(Level.SEVERE, null, ex);
+public class BaseDAO {
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query = "Select * from product";
+        try{
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                list.add(new Product(   rs.getInt(1), 
+                                    rs.getString(2), 
+                                    rs.getString(3), 
+                                    rs.getDouble(4), 
+                                    rs.getString(6)));
+            }
+        }catch (Exception e){
         }
+        
+        return list;
     }
     
-    public abstract ArrayList<T> getStudents();
+    public List<Category> getCategory(){
+        List<Category> list = new ArrayList<>();
+        String query = "Select * from Category";
+        try{
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                list.add(new Category(   rs.getInt(1), 
+                                    rs.getString(2)));
+            }
+        }catch (Exception e){
+        }
+        
+        return list;
+    }
+    
+    public static void main(String[] args) {
+        BaseDAO dao = new BaseDAO();
+        List<Category> list =  dao.getCategory();
+        for(Category p: list){
+            System.out.println(p);
+        }
+    }
     
 }
